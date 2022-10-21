3b6a19506bb269e9f8df82d8d3d347dda0085543
Chao Liu <lc.roy86@gmail.com>
2022-10-21 16:13:13 -0600

    Update docker to ROCm5.3 (#2)
    * upgrade compiler to ROCM 5.3 version
    
    * remove unnecessary build fixes
    
    Co-authored-by: illsilin <Illia.Silin@amd.com>

--- docker/rocm_fix/fix_10736.py
+++
@@ -1,9 +0,0 @@
-src = ""
-with open("/opt/rocm/hip/bin/hipcc.pl", "r") as fi:
-    src = fi.read()
-
-src = src.replace(
-    "$HIP_CLANG_TARGET = chomp($HIP_CLANG_TARGET);", "chomp($HIP_CLANG_TARGET);"
-)
-with open("/opt/rocm/hip/bin/hipcc.pl", "w") as fo:
-    fo.write(src)
