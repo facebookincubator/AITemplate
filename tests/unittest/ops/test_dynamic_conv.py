c2b03f076af7e5750bca690614881f25b483dd8e
Max Podkorytov <maxdp@meta.com>
2022-11-14 17:52:14 -0800

    add missing copyright headers (#86)

--- tests/unittest/ops/test_dynamic_conv.py
+++ tests/unittest/ops/test_dynamic_conv.py
@@ -1,4 +1,4 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+#  Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
