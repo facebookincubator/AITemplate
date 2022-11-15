726c581487b23bdcc80199f6c205ea3b071922ec
Max Podkorytov <maxdp@fb.com>
2022-11-15 01:31:30 +0000

    add missing copyright headers

--- python/aitemplate/backend/cuda/conv3d/conv3d.py
+++ python/aitemplate/backend/cuda/conv3d/conv3d.py
@@ -1,4 +1,4 @@
-#  Copyright (c) Meta Platform, Inc. and its affiliates.
+#  Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+
 """
 Codegen for conv3d.
 """
