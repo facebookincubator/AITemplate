c3a0de742439e6d89ba3a065b04a2f9b78def937
Max Podkorytov <maxdp@meta.com>
2023-03-02 22:37:35 -0800

    fix profiler timeout message
    Summary: as titled
    
    Differential Revision: D43762214
    
    fbshipit-source-id: eedb07130c509eef4b87628e36856b4b3d6f0394

--- python/aitemplate/backend/profiler_runner.py
+++ python/aitemplate/backend/profiler_runner.py
@@ -261,6 +261,7 @@ class ProfilerRunner:
         self._timeout = timeout
         self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(devices))
         self._futures = []
+        self._cmds = []
         self._postprocessing_delegate = postprocessing_delegate
         try:
             target = Target.current()
@@ -311,6 +312,7 @@ class ProfilerRunner:
 
         future.add_done_callback(callback_when_done)
         self._futures.append(future)
+        self._cmds.append(cmds)
 
     def join(self):
         """
@@ -318,7 +320,19 @@ class ProfilerRunner:
         """
         done, not_done = concurrent.futures.wait(self._futures, self._timeout)
         for f in not_done:
+            # attempts cancelling, will fail if call is being executed or has finished
             f.cancel()
-        for _ in self._futures:
+        cancelled_cmds = [
+            cmd for cmd, f in zip(self._cmds, self._futures) if f.cancelled()
+        ]
+        if cancelled_cmds:
+            raise RuntimeError(
+                f"Profiler timed out after {self._timeout} sec. "
+                "Try increasing the timeout. "
+                f"Cancelled profilers: {cancelled_cmds}"
+            )
+        for _ in [f for f in self._futures if f.done() or f.running()]:
+            # sync point between futures and queue.
+            # wait for callbacks to finish
             self._done_queue.get(timeout=self._timeout)
         self._postprocessing_delegate.postprocess_results()
