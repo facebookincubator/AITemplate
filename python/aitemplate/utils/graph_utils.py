13456956ef7693d365a938d0a4a809257a0dd7c0
Alexandr Guzhva <aguzhva@meta.com>
2023-03-21 07:13:34 -0700

    Improve graph visualization
    Summary:
    Creates an additional graph visualization in case of profiling. Becomes available once `LOGLEVEL=DEBUG` is used, and it is recommended to enable  `AIT_PLOT_SHORTEN_TENSOR_NAMES=1` as well.
    
    Highlights ops with the duration higher than 70% percentile.
    
    Every op gets marked with an execution duration. Size is proportional to the duration for the above 70% percentile.
    
    Every tensor gets two timestamps in the form of 'A ms / B ms'. A is the timestamp when the tensor becomes computed using a single stream execution. B is the timestamp when the tensor becomes computed using unlimited number of independent streams, thus serving as a theoretically minimal timestamp at which the tensor may be computed.
    
    The ratio of `A/B - 1` for the output tensor is how much speed improvement one may gain in case of unlimited amount of GPUs available and zero copy cost.
    
    Differential Revision: D44183018
    
    fbshipit-source-id: cdd6a5448c0d02d80c545f1a3478ad971c3f1695

--- python/aitemplate/utils/graph_utils.py
+++ python/aitemplate/utils/graph_utils.py
@@ -12,9 +12,11 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+import json
 import logging
 import os
-from typing import Any, List
+from collections import deque
+from typing import Any, Dict, List, Set
 
 from aitemplate.utils.misc import is_debug
 from aitemplate.utils.visualization import plot_graph
@@ -24,6 +26,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def get_sorted_ops(tensors) -> List[Any]:
+    """
+    Produces the exact execution sequence of operators.
+    This matches backend/codegen.py, ModelContainerGenerator.append_all_tensors()
+    """
+
     from aitemplate.compiler.base import Tensor
 
     visited = set()
@@ -31,7 +38,7 @@ def get_sorted_ops(tensors) -> List[Any]:
     if isinstance(tensors, Tensor):
         tensors = [tensors]
     for tensor in tensors:
-        for src_op in tensor._attrs["src_ops"]:
+        for src_op in tensor.src_ops():
             if src_op in visited:
                 continue
             visited.add(src_op)
@@ -84,7 +91,7 @@ def sorted_op_pseudo_code(ops, with_shape=True) -> str:
     return op_str
 
 
-def dump_graph_debug_str_to_file(tensors, workdir, name):
+def dump_graph_debug_str_to_file(tensors, workdir, name, file_with_time_profiles=None):
     if is_debug():
         # Dump graph and pseudo code for debug only
         prefix = os.path.join(workdir, name)
@@ -101,5 +108,276 @@ def dump_graph_debug_str_to_file(tensors, workdir, name):
         with open(pseudo_code_path, "w") as f:
             f.write(sorted_graph_pseudo_code(tensors))
             _LOGGER.debug(f"Dumped {name} pseudo code to {pseudo_code_path}")
-        plot_graph(tensors, graph_visual_path)
+        plot_graph(tensors, graph_visual_path, file_with_time_profiles)
         _LOGGER.debug(f"Dumped {name} visualization to {graph_visual_path}")
+
+
+class TimestampTracking:
+    def __init__(
+        self, execution_start: float = 0, duration: float = 0, execution_order: int = 0
+    ):
+        self.execution_order = execution_order
+        self.execution_start = execution_start
+        self.duration = duration
+
+    @property
+    def execution_end(self):
+        return self.execution_start + self.duration
+
+
+class ProfiledTimeStatistics:
+    def __init__(self):
+        # Dict[Operator, float]
+        self.op_durations = {}
+
+        # Dict[Operator, TimestampTracking]
+        self.op_parallel_trackers = {}
+        # Dict[Operator, TimestampTracking]
+        self.op_sequential_trackers = {}
+
+        # Dict[Trnsor, TimestampTracking]
+        self.tensor_parallel_trackers = {}
+        # Dict[Trnsor, TimestampTracking]
+        self.tensor_sequential_trackers = {}
+
+        # 0.7 percentile of op times
+        self.duration_p70 = 0.0
+        # 0.9 percentile of op times
+        self.duration_p90 = 0.0
+        # 0.95 percentile of op times
+        self.duration_p95 = 0.0
+        # max time spent among operators
+        self.duration_max = 0.0
+        # total time spent by operators
+        self.total_duration = 0.0
+
+
+def track_graph_timings(
+    tensors, file_path_profiler_output: str
+) -> ProfiledTimeStatistics:
+    """
+    Traverses the graph of tensors and uses the statistics from the profiler
+    to evaluate execution times in case of sequential execution (1 stream)
+    and parallel execution (unlimited number of streams).
+    """
+
+    from aitemplate.compiler.base import Operator, Tensor
+
+    output = ProfiledTimeStatistics()
+
+    # the exact sequence of non-constant tensors that need to be evaluated
+    #   within a single execution stream.
+    unprocessed_tensors: List[Tensor] = []
+
+    # Sequence_of_ops contains an exact execution sequence of ops
+    #   within a single execution stream.
+    # Similar to graph_utils.py, get_sorted_ops() call.
+    sequence_of_ops: List[Operator] = []
+    visited_ops: Set[Operator] = set()
+
+    for tensor in tensors:
+        src_ops = tensor.src_ops()
+
+        if len(src_ops) == 0:
+            # This tensor depends on no operator.
+            # So, add the final statistics for it.
+            output.tensor_parallel_trackers[tensor] = TimestampTracking()
+            output.tensor_sequential_trackers[tensor] = TimestampTracking()
+        else:
+            for op in src_ops:
+                if op not in visited_ops:
+                    visited_ops.add(op)
+                    sequence_of_ops.append(op)
+
+            # this tensor needs to be evaluated
+            unprocessed_tensors.append(tensor)
+
+    # ok, we've got ops. Load the file with the profile.
+    with open(file_path_profiler_output, "r") as f:
+        perf_per_op_str = f.read()
+
+    # parse file
+    perf_per_op_str_dict = json.loads(perf_per_op_str)
+
+    op_durations: Dict[str, float] = {}
+    for op_name, op_data in perf_per_op_str_dict.items():
+        op_durations[op_name] = op_data["ms_per_iter"]
+
+    # map timings to ops
+    for op in visited_ops:
+        # profiler records the results under the original_name
+        op_name = op._attrs["original_name"]
+
+        # replace op_name with an unique name, if provided
+        if op_name is not None:
+            if op_name not in op_durations:
+                # op_name was not found in the profiler report
+                output.op_durations[op] = 0
+            else:
+                time_cost = op_durations[op_name]
+                output.op_durations[op] = time_cost
+        else:
+            # op_name is None, idk what to do
+            output.op_durations[op] = 0
+
+    # compute statistics
+    sorted_op_durations = sorted(op_durations.values())
+    if len(sorted_op_durations) > 0:
+        output.duration_p70 = sorted_op_durations[int(len(sorted_op_durations) * 0.7)]
+        output.duration_p90 = sorted_op_durations[int(len(sorted_op_durations) * 0.9)]
+        output.duration_p95 = sorted_op_durations[int(len(sorted_op_durations) * 0.95)]
+        output.duration_max = sorted_op_durations[-1]
+        output.total_duration = sum(sorted_op_durations)
+
+    # proceed with sequential execution:
+    unprocessed_seq_ops = deque(sequence_of_ops)
+    unprocessed_seq_tensors = deque(unprocessed_tensors)
+
+    global_timestamp = 0.0
+    execution_step = 0
+    while len(unprocessed_seq_ops) > 0 or len(unprocessed_seq_tensors) > 0:
+        # process operators
+        n_local_processed_ops = 0
+        for op in unprocessed_seq_ops:
+            depends_on = op._attrs["inputs"]
+
+            # are all prereqs complete?
+            can_proceed = all(
+                tensor in output.tensor_sequential_trackers for tensor in depends_on
+            )
+            if can_proceed:
+                # yes. This operator is ready to be executed.
+                execution_step += 1
+
+                op_duration = output.op_durations[op]
+
+                output.op_sequential_trackers[op] = TimestampTracking(
+                    execution_start=global_timestamp,
+                    duration=op_duration,
+                    execution_order=execution_step,
+                )
+
+                # modify global clock
+                global_timestamp += op_duration
+
+                n_local_processed_ops += 1
+            else:
+                # cannot go ahead, some tensors need to be marked as processed
+                break
+
+        for _ in range(0, n_local_processed_ops):
+            unprocessed_seq_ops.popleft()
+
+        # process tensors
+        n_local_processed_tensors = 0
+        for tensor in unprocessed_seq_tensors:
+            depends_on = tensor.src_ops()
+
+            # are all prereqs complete?
+            can_proceed = all(op in output.op_sequential_trackers for op in depends_on)
+            if can_proceed:
+                # yes. The tensor computation is finished.
+                max_execution_end = max(
+                    output.op_sequential_trackers[op].execution_end for op in depends_on
+                )
+                max_execution_order = max(
+                    output.op_sequential_trackers[op].execution_order
+                    for op in depends_on
+                )
+
+                output.tensor_sequential_trackers[tensor] = TimestampTracking(
+                    execution_start=max_execution_end,
+                    duration=0.0,
+                    execution_order=max_execution_order,
+                )
+
+                n_local_processed_tensors += 1
+            else:
+                # cannot proceed, some ops needs to be run first
+                break
+
+        for _ in range(0, n_local_processed_tensors):
+            unprocessed_seq_tensors.popleft()
+
+        # are we done?
+        if n_local_processed_ops == 0 and n_local_processed_tensors == 0:
+            # yes, no operators or tensors were processed on the current step.
+            # This does not imply that all operators and tensors were processed.
+            # Basically, this is a kinda early termination verification that
+            # indicates that there is some invalid profiler / graph data.
+            # So, we're trying to avoid infinite loops.
+            break
+
+    # process with parallel execution
+    unprocessed_par_ops = set(sequence_of_ops)
+    unprocessed_par_tensors = set(unprocessed_tensors)
+
+    execution_step = 0
+    while len(unprocessed_par_ops) > 0 or len(unprocessed_par_tensors) > 0:
+        # process operators
+        new_processed_ops: Set[Operator] = set()
+        for op in unprocessed_par_ops:
+            depends_on = op._attrs["inputs"]
+
+            # are all prereqs complete?
+            can_proceed = all(
+                tensor in output.tensor_parallel_trackers for tensor in depends_on
+            )
+            if can_proceed:
+                # yes. This operator is ready to be executed.
+                op_duration = output.op_durations[op]
+
+                max_execution_end = max(
+                    output.tensor_parallel_trackers[tensor].execution_end
+                    for tensor in depends_on
+                )
+
+                output.op_parallel_trackers[op] = TimestampTracking(
+                    execution_start=max_execution_end,
+                    duration=op_duration,
+                    execution_order=execution_step,
+                )
+
+                new_processed_ops.add(op)
+
+        # ok, there were some processed operators
+        if len(new_processed_ops) > 0:
+            for op in new_processed_ops:
+                unprocessed_par_ops.remove(op)
+
+            execution_step += 1
+
+        # process tensors
+        new_processed_tensors: Set[Tensor] = set()
+        for tensor in unprocessed_par_tensors:
+            depends_on = tensor.src_ops()
+
+            # are all prereqs complete?
+            can_proceed = all(op in output.op_parallel_trackers for op in depends_on)
+            if can_proceed:
+                # yes. The tensor computation is finished.
+                max_execution_end = max(
+                    output.op_parallel_trackers[op].execution_end for op in depends_on
+                )
+                max_execution_order = max(
+                    output.op_parallel_trackers[op].execution_order for op in depends_on
+                )
+
+                output.tensor_parallel_trackers[tensor] = TimestampTracking(
+                    execution_start=max_execution_end,
+                    duration=0.0,
+                    execution_order=max_execution_order,
+                )
+
+                new_processed_tensors.add(tensor)
+
+        for tensor in new_processed_tensors:
+            unprocessed_par_tensors.remove(tensor)
+
+        # are we done?
+        if len(new_processed_ops) == 0 and len(new_processed_tensors) == 0:
+            # Same story: we're trying to avoid infinite loops.
+            break
+
+    # done
+    return output
--- python/aitemplate/utils/visualization/plot.py
+++ python/aitemplate/utils/visualization/plot.py
@@ -18,6 +18,8 @@ Graph visualization tool for AITemplate
 import json
 import os
 
+from typing import Optional
+
 from aitemplate import compiler
 from aitemplate.utils.environ import shorten_tensor_names_for_plots
 from aitemplate.utils.misc import short_str
@@ -92,7 +94,31 @@ def _gen_op_modal(op) -> str:
     return modal_src
 
 
-def plot_graph(tensors, file_path: str) -> None:
+def _highlight_op_node(op_node, op, time_stats):
+    if op in time_stats.op_durations:
+        perf_op = time_stats.op_durations[op]
+        scale_factor = float(perf_op) / float(time_stats.total_duration)
+
+        if perf_op > time_stats.duration_p95:
+            op_node.set("color", "maroon1")
+            op_node.set("penwidth", 9)
+            op_node.set("width", 1 + scale_factor * 100)
+            op_node.set("height", 1 + scale_factor * 50)
+        elif perf_op > time_stats.duration_p90:
+            op_node.set("color", "magenta1")
+            op_node.set("penwidth", 6)
+            op_node.set("width", 1 + scale_factor * 100)
+            op_node.set("height", 1 + scale_factor * 50)
+        elif perf_op > time_stats.duration_p70:
+            op_node.set("color", "mediumorchid1")
+            op_node.set("penwidth", 3)
+            op_node.set("width", 1 + scale_factor * 100)
+            op_node.set("height", 1 + scale_factor * 50)
+
+
+def plot_graph(
+    tensors, file_path: str, file_with_time_profiles: Optional[str] = None
+) -> None:
     """
     Plot AIT graph.
 
@@ -104,6 +130,9 @@ def plot_graph(tensors, file_path: str) -> None:
         Output file path, currently we support the following extension:
             - html
             - format supported by graphviz
+    file_with_time_profile : Optional[str]
+        Adds time for every node, if provided
+
     """
     dot_graph = pydot.Dot(graph_type="digraph")
     _, ext = os.path.splitext(file_path)
@@ -130,6 +159,12 @@ def plot_graph(tensors, file_path: str) -> None:
 
     op_names = gen_unique_op_names(sorted_graph)
 
+    from aitemplate.utils.graph_utils import ProfiledTimeStatistics, track_graph_timings
+
+    time_stats = ProfiledTimeStatistics()
+    if file_with_time_profiles is not None:
+        time_stats = track_graph_timings(sorted_graph, file_with_time_profiles)
+
     op_set = {}
     tensor_set = {}
     modal_set = []
@@ -138,9 +173,6 @@ def plot_graph(tensors, file_path: str) -> None:
     for tensor in sorted_graph:
         tensor_node = None
         tensor_name = tensor._attrs["name"]
-        if shorten_tensor_names_for_plots():
-            if tensor_name is not None and len(tensor_name) > 30:
-                tensor_name = short_str(tensor_name)
 
         if tensor in tensor_set:
             tensor_node = tensor_set[tensor]
@@ -152,17 +184,42 @@ def plot_graph(tensors, file_path: str) -> None:
                 color = COLOR_SCHEME["output"]
             if tensor._attrs["is_param"] is True:
                 color = COLOR_SCHEME["param"]
+
+            label = tensor_name
+
+            if shorten_tensor_names_for_plots():
+                if tensor_name is not None and len(tensor_name) > 30:
+                    label = short_str(tensor_name)
+
+            # add a label with time
+            label_with_time = ""
+            seq_tracker = time_stats.tensor_sequential_trackers.get(tensor, None)
+            if seq_tracker is not None and seq_tracker.execution_end != 0:
+                label_with_time += f"{seq_tracker.execution_end:.3f} ms"
+
+            par_tracker = time_stats.tensor_parallel_trackers.get(tensor, None)
+            if par_tracker is not None and par_tracker.execution_end != 0:
+                if label_with_time:
+                    label_with_time += " / "
+                label_with_time += f"{par_tracker.execution_end:.3f} ms"
+
+            if label_with_time:
+                label = f"{tensor_name}\\n{label_with_time}"
+
+            # add a node
             tensor_node = pydot.Node(
                 name=tensor_name,
                 shape="note",
                 id=tensor_name,
+                label=label,
                 color=color,
             )
             tensor_set[tensor] = tensor_node
             dot_graph.add_node(tensor_node)
             modal_set.append(_gen_tensor_modal(tensor))
             items.append(tensor_name)
-            popover_data[tensor_name] = "shape: " + _get_tensor_shape_str(tensor)
+
+            popover_data[tensor_name] = f"shape: {_get_tensor_shape_str(tensor)}"
 
         for src_op in tensor.src_ops():
             op_node = None
@@ -175,12 +232,20 @@ def plot_graph(tensors, file_path: str) -> None:
             if src_op in op_set:
                 op_node = op_set[src_op]
             else:
+                label = (
+                    f"{op_name}\\n{str(time_stats.op_durations[src_op])} ms"
+                    if src_op in time_stats.op_durations
+                    else op_name
+                )
                 op_node = pydot.Node(
                     name=op_name,
                     shape="folder",
                     id=op_name,
+                    label=label,
                     color="mediumpurple1",
                 )
+                _highlight_op_node(op_node, src_op, time_stats)
+
                 op_set[src_op] = op_node
                 dot_graph.add_node(op_node)
                 modal_set.append(_gen_op_modal(src_op))
@@ -201,15 +266,24 @@ def plot_graph(tensors, file_path: str) -> None:
             if dst_op in op_set:
                 op_node = op_set[dst_op]
             else:
+                label = (
+                    f"{op_name}\\n{str(time_stats.op_durations[dst_op])} ms"
+                    if dst_op in time_stats.op_durations
+                    else op_name
+                )
                 op_node = pydot.Node(
                     name=op_name,
                     shape="folder",
                     id=op_name,
+                    label=label,
                     color="mediumpurple1",
                 )
+                _highlight_op_node(op_node, dst_op, time_stats)
+
                 op_set[dst_op] = op_node
                 dot_graph.add_node(op_node)
                 items.append(op_name)
+
                 popover_data[op_name] = ", ".join(
                     [f"{x}: {y}" for x, y in op_to_content(dst_op).items()]
                 )
