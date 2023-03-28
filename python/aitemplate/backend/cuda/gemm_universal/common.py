9bd086cd80902a42fa1bfad740ca8c578fcc3277
Alexander Pivovarov <apivovarov@gmail.com>
2023-03-27 23:56:18 -0700

    Remove links to youtube debug background music (#496)
    Summary:
    Found interesting links in gemm profiling templates: https://www.youtube.com/watch?v=rRwxfYlgG-M
    
    Let's add the description - Namewee - China REGGAETON
    
    Pull Request resolved: https://github.com/facebookincubator/AITemplate/pull/496
    
    Reviewed By: frank-wei
    
    Differential Revision: D44395422
    
    Pulled By: chenyang78
    
    fbshipit-source-id: 5dfd7516f599573e505f24a57c1fa2ad0be56ebb

--- python/aitemplate/backend/cuda/gemm_universal/common.py
+++ python/aitemplate/backend/cuda/gemm_universal/common.py
@@ -222,7 +222,6 @@ EXEC_TEMPLATE = jinja2.Template(
 
 {{indent}}};
 {% if is_profiler %}
-{{indent}}// https://www.youtube.com/watch?v=rRwxfYlgG-M
 {{indent}}size_t workspace_size = gemm_op.get_workspace_size(arguments);
 {{indent}}cutlass::device_memory::allocation<uint8_t> local_workspace(workspace_size);
 {{indent}}workspace = local_workspace.get();
