383f516af7079bd874b49dc9f34185c87710ba8f
fsx950223 <fsx950223@outlook.com>
2023-03-31 13:45:24 +0800

    print op name when raise runtime error

--- python/aitemplate/compiler/ops/groupnorm/groupnorm.py
+++ python/aitemplate/compiler/ops/groupnorm/groupnorm.py
@@ -265,7 +265,7 @@ class group_norm(Operator):
 
         if len(result) == 0:
             raise RuntimeError(
-                "Profile workload: " f"{exec_key}" " failed. " f"Results: {result}."
+                "Profile workload: " f"{self._attrs['op']}" f"{exec_key}" " failed. " f"Results: {result}."
             )
 
         out = min(result, key=lambda x: x[1].duration)
