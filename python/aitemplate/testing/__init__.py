27ec1e1122f2888180526c6924f7da7ce5740dc7
Max Podkorytov <maxdp@meta.com>
2023-04-14 11:55:04 -0700

    implement per op benchmark backbone (#555)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/AITemplate/pull/555
    
    Ship the module profiling utility externally
    
    Reviewed By: wushirong
    
    Differential Revision: D44567322
    
    fbshipit-source-id: 4f8ca36dbdc72dfa60e667c3592d0a2bc466b994

--- python/aitemplate/testing/__init__.py
+++ python/aitemplate/testing/__init__.py
@@ -17,9 +17,6 @@ testing module
 """
 from aitemplate.testing import benchmark_ait, benchmark_pt
 from aitemplate.testing.detect_target import detect_target
+from aitemplate.testing.profile import profile_callable
 
-__all__ = [
-    "benchmark_pt",
-    "benchmark_ait",
-    "detect_target",
-]
+__all__ = ["benchmark_pt", "benchmark_ait", "detect_target", "profile_callable"]
