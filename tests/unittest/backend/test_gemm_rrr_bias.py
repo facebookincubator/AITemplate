9d3c95092eea22031c00e495d9c7e62c36a93ae1
Chengxu Yang <yangchengxu@bytedance.com>
2023-06-30 10:59:52 +0800

    remove unused import

--- tests/unittest/backend/test_gemm_rrr_bias.py
+++ tests/unittest/backend/test_gemm_rrr_bias.py
@@ -21,7 +21,6 @@ import unittest
 import torch
 
 from aitemplate.compiler import compile_model, ops
-from aitemplate.compiler.ops.common.epilogue import FuncEnum
 from aitemplate.frontend import IntImm, Tensor
 from aitemplate.testing import detect_target
 from aitemplate.testing.test_utils import (
