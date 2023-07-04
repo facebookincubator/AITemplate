c2449a6dda2d248c498a7c60233b59f350674290
yangchengxu <yangchengxu@bytedance.com>
2023-07-04 15:53:58 +0800

    remove CUTLASS 3.x-related templates

--- python/aitemplate/backend/cuda/gemm_universal/gemm_rrr_bias.py
+++ python/aitemplate/backend/cuda/gemm_universal/gemm_rrr_bias.py
@@ -63,53 +63,6 @@ PROBLEM_ARGS_TEMPLATE = jinja2.Template(
 )
 
 
-# in case of TMA epilogue schedule, use the transposed problem to pass the
-# column-major bias vector through the bias + elementwise epilogue (not residual)
-PROBLEM_ARGS_TEMPLATE_CUTLASS_3X = jinja2.Template(
-    """
-    cutlass::gemm::GemmUniversalMode::kGemm,                     // GemmUniversalMode mode
-{% if has_tma_epilogue %}
-    {
-        static_cast<coord_t>(N),
-        static_cast<coord_t>(M),
-        static_cast<coord_t>(K),
-        static_cast<coord_t>(1)
-    },                                                           // ProblemShape problem_shape
-    ({{elem_input_type}}*)(b_ptr) + input_b_offset,              // ElementA const* ptr_A
-    {input_b_stride, cute::Int<1>{}, cute::Int<0>{}},            // StrideA dA
-    ({{elem_input_type}}*)(a_ptr) + input_a_offset,              // ElementB const* ptr_B
-    {input_a_stride, cute::Int<1>{}, cute::Int<0>{}},            // StrideB dB
-    {
-        {ElementComputeEpilogue(1), ElementComputeEpilogue(0)},  // typename ThreadEpilogueOp::Params thread
-        nullptr,                                                 // ElementC const* ptr_C
-        {cute::Int<1>{}, cute::Int<0>{}, cute::Int<0>{}},        // StrideC dC
-        ({{elem_output_type}}*)(c_ptr) + output_offset,          // ElementD const* ptr_D
-        {cute::Int<1>{}, output_stride, cute::Int<0>{}},         // StrideD dD
-        ({{elem_input_type}}*)(bias_ptr),                        // ElementBias const* ptr_Bias
-    },                                                           // EpilogueArguments epilogue
-{% else %}
-    {
-        static_cast<coord_t>(M),
-        static_cast<coord_t>(N),
-        static_cast<coord_t>(K),
-        static_cast<coord_t>(1)
-    },                                                           // ProblemShape problem_shape
-    ({{elem_input_type}}*)(a_ptr) + input_a_offset,              // ElementA const* ptr_A
-    {input_a_stride, cute::Int<1>{}, cute::Int<0>{}},            // StrideA dA
-    ({{elem_input_type}}*)(b_ptr) + input_b_offset,              // ElementB const* ptr_B
-    {input_b_stride, cute::Int<1>{}, cute::Int<0>{}},            // StrideB dB
-    {
-        {ElementComputeEpilogue(1), ElementComputeEpilogue(1)},  // typename ThreadEpilogueOp::Params thread
-        ({{elem_input_type}}*)(bias_ptr),                        // ElementC const* ptr_C
-        {cute::Int<0>{}, cute::Int<1>{}, cute::Int<0>{}},        // StrideC dC
-        ({{elem_output_type}}*)(c_ptr) + output_offset,          // ElementD const* ptr_D
-        {output_stride, cute::Int<1>{}, cute::Int<0>{}},         // StrideD dD
-    },                                                           // EpilogueArguments epilogue
-{% endif %}
-"""
-)
-
-
 # for profiler, no need to include TensorAccessor
 PROFILER_PROBLEM_ARGS_TEMPLATE = jinja2.Template(
     """
@@ -137,74 +90,9 @@ PROFILER_PROBLEM_ARGS_TEMPLATE = jinja2.Template(
 )
 
 
-# in case of TMA epilogue schedule, use the transposed problem to pass the
-# column-major bias vector through the bias + elementwise epilogue (not residual)
-PROFILER_PROBLEM_ARGS_TEMPLATE_CUTLASS_3X = jinja2.Template(
-    """
-    cutlass::gemm::GemmUniversalMode::kGemm,                     // GemmUniversalMode mode
-{% if has_tma_epilogue %}
-    {
-        static_cast<coord_t>(N),
-        static_cast<coord_t>(M),
-        static_cast<coord_t>(K),
-        static_cast<coord_t>(1)
-    },                                                           // ProblemShape problem_shape
-    ({{elem_input_type}}*)(b_ptr),                               // ElementA const* ptr_A
-    {K, cute::Int<1>{}, cute::Int<0>{}},                         // StrideA dA
-    ({{elem_input_type}}*)(a_ptr),                               // ElementB const* ptr_B
-    {N, cute::Int<1>{}, cute::Int<0>{}},                         // StrideB dB
-    {
-        {ElementComputeEpilogue(1), ElementComputeEpilogue(0)},  // typename ThreadEpilogueOp::Params thread
-        nullptr,                                                 // ElementC const* ptr_C
-        {cute::Int<1>{}, cute::Int<0>{}, cute::Int<0>{}},        // StrideC dC
-        ({{elem_output_type}}*)(c_ptr) + output_offset,          // ElementD const* ptr_D
-        {cute::Int<1>{}, output_stride, cute::Int<0>{}},         // StrideD dD
-        ({{elem_input_type}}*)(bias_ptr),                        // ElementBias const* ptr_Bias
-    },                                                           // EpilogueArguments epilogue
-{% else %}
-    {
-        static_cast<coord_t>(M),
-        static_cast<coord_t>(N),
-        static_cast<coord_t>(K),
-        static_cast<coord_t>(1)
-    },                                                           // ProblemShape problem_shape
-    ({{elem_input_type}}*)(a_ptr),                               // ElementA const* ptr_A
-    {K, cute::Int<1>{}, cute::Int<0>{}},                         // StrideA dA
-    ({{elem_input_type}}*)(b_ptr),                               // ElementB const* ptr_B
-    {N, cute::Int<1>{}, cute::Int<0>{}},                         // StrideB dB
-    {
-        {ElementComputeEpilogue(1), ElementComputeEpilogue(1)},  // typename ThreadEpilogueOp::Params thread
-        ({{elem_input_type}}*)(bias_ptr),                        // ElementC const* ptr_C
-        {cute::Int<0>{}, cute::Int<1>{}, cute::Int<0>{}},        // StrideC dC
-        ({{elem_output_type}}*)(c_ptr) + output_offset,          // ElementD const* ptr_D
-        {output_stride, cute::Int<1>{}, cute::Int<0>{}},         // StrideD dD
-    },                                                           // EpilogueArguments epilogue
-{% endif %}
-"""
-)
-
-
 @registry.reg("cuda.gemm_rrr_bias.config")
 def gemm_rrr_config(func_attrs, dtype="float16"):
-    common.make_fproc(func_attrs, RRR, include_cutlass_3x_ops=True)
-
-    import cutlass_lib
-
-    for op in func_attrs["op_instance"].values():
-        if common.has_tma_epilogue(op):
-            # disable residual to leave more SMEM for the mainloop
-            op.C.element = cutlass_lib.library.DataType.void
-
-            # swap the output layout to the transposed problem
-            op.C.layout = cutlass_lib.library.LayoutType.ColumnMajor
-            op.D.layout = cutlass_lib.library.LayoutType.ColumnMajor
-
-            # switch to a TMA epilogue with bias
-            op.epilogue_schedule = (
-                cutlass_lib.library.EpilogueScheduleBiasElementwiseMapping[
-                    op.epilogue_schedule
-                ]
-            )
+    common.make_fproc(func_attrs, RRR)
 
 
 @registry.reg("cuda.gemm_rrr_bias.gen_profiler")
@@ -227,7 +115,6 @@ def gen_profiler(func_attrs, workdir, profiler_filename, dim_info_dict):
         dim_info_dict=dim_info_dict,
         src_template=common_bias.SRC_TEMPLATE,
         problem_args_template=PROFILER_PROBLEM_ARGS_TEMPLATE,
-        problem_args_template_cutlass_3x=PROFILER_PROBLEM_ARGS_TEMPLATE_CUTLASS_3X,
         bias_ptr_arg="memory_pool->RequestTensorByIdx(3)",
         extra_code=extra_code,
     )
@@ -254,14 +141,6 @@ def gen_function(
         elem_input_type=elem_input_type,
         elem_output_type=elem_output_type,
     )
-    problem_args_cutlass_3x = PROBLEM_ARGS_TEMPLATE_CUTLASS_3X.render(
-        elem_input_type=elem_input_type,
-        elem_output_type=elem_output_type,
-        has_tma_epilogue=any(
-            common.has_tma_epilogue(func_attrs["op_instance"][exec_item.algo])
-            for exec_item in func_attrs["exec_path"].values()
-        ),
-    )
     extra_code = EXTRA_CODE.render(
         elem_input_type=elem_input_type,
         elem_output_type=elem_output_type,
@@ -271,7 +150,6 @@ def gen_function(
         src_template=common_bias.SRC_TEMPLATE,
         exec_cond_template=exec_cond_template,
         problem_args=problem_args,
-        problem_args_cutlass_3x=problem_args_cutlass_3x,
         input_ndims=input_ndims,
         weight_ndims=weight_ndims,
         output_ndims=output_ndims,
