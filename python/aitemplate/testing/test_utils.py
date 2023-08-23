55c62d1d16977e2eb33ce94385875326f8616322
Colin Chan <ctyc@meta.com>
2023-08-23 12:33:36 -0700

    Add benchmark_module (#911)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/AITemplate/pull/911
    
    Title
    
    Follow up of comment from qxy11 https://www.internalfb.com/diff/D48447922?dst_version_fbid=269587765855811&transaction_fbid=809047260924856.
    
    Reviewed By: aakhundov
    
    Differential Revision: D48606705
    
    fbshipit-source-id: 3462df88fa6431ffef1aaebfcfa3565c17fa6f2a

--- python/aitemplate/testing/test_utils.py
+++ python/aitemplate/testing/test_utils.py
@@ -22,11 +22,14 @@ import unittest
 from enum import Enum
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Type
 
+import aitemplate
+
 import torch
 
 from aitemplate.compiler.base import IntImm, IntVar, Operator, Tensor
 from aitemplate.compiler.dtype import normalize_dtype
 from aitemplate.compiler.ops.b2b_bmm.b2b_bmm_base import CausalType
+from aitemplate.testing.benchmark_pt import benchmark_torch_function
 from aitemplate.testing.detect_target import detect_target
 from aitemplate.utils.graph_utils import get_sorted_ops
 from aitemplate.utils.torch_utils import string_to_torch_dtype
@@ -313,3 +316,47 @@ def init_random_weights(m):
         pass
     else:
         print("Passed root module: " + str(type(m)))
+
+
+def benchmark_module(
+    name: str,
+    inputs: Tensor,
+    outputs: Tensor,
+    pt_mod: torch.nn.Module,
+    ait_mod: aitemplate.frontend.nn.module.Module,
+    iters: int = 100,
+    permute_inputs: Optional[List[int]] = None,
+):
+    input_shape = inputs.size()
+    batch_size = input_shape[0]
+
+    # benchmark pt
+    # warmup
+    args = (inputs,)
+    t = benchmark_torch_function(iters, pt_mod, *args)
+    # benchmark
+    t_pt = benchmark_torch_function(iters, pt_mod, *args)
+    print(
+        f"{name}_pt: batch_size: {batch_size}, {t_pt} ms",
+    )
+
+    # benchmark ait
+    # warm up
+    inputs = inputs.permute(permute_inputs).contiguous() if permute_inputs else inputs
+    graph_mode = False
+    t, _, __ = ait_mod.benchmark_with_tensors(
+        [inputs],
+        [outputs],
+        count=iters,
+        graph_mode=graph_mode,
+    )
+    # benchmark
+    t_ait, _, __ = ait_mod.benchmark_with_tensors(
+        [inputs],
+        [outputs],
+        count=iters,
+        graph_mode=graph_mode,
+    )
+    print(
+        f"{name}_ait: batch_size: {batch_size}, {t_ait} ms",
+    )
